3d149060094ea7cf9d71035e47ab7b9adb07a35d
Kiddie22 <rasula.20191118@iit.ac.lk>
2021-03-14 15:45:23 +0530

    added city selection

--- weatherman/src/components/TileList.js
+++ weatherman/src/components/TileList.js
@@ -2,13 +2,14 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Tile from "./Tile";
 import { SimpleGrid } from "@chakra-ui/react";
+import { cities } from "../cities";
 
 const TileList = () => {
-  const [city, setCity] = useState({});
+  const [query, setQuery] = useState("galle");
+  const [city, setCity] = useState([]);
   const [list, setList] = useState([]);
 
-  const URL =
-    "http://api.openweathermap.org/data/2.5/forecast?q=galle&appid=01449de77e3668d9b85822879d4b13f1&units=metric&cnt=20";
+  const URL = `http://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=01449de77e3668d9b85822879d4b13f1&units=metric&cnt=20`;
 
   useEffect(() => {
     fetch(URL)
@@ -20,13 +21,24 @@ const TileList = () => {
         setCity(city);
         setList(list);
       });
-  }, []);
+  }, [query]);
+
+  const confirmSelection = () => {
+    setQuery(document.getElementById("city").value);
+  };
 
   return (
     <React.Fragment>
       <select name="city" id="city">
-
+        {cities.map((city) => {
+          return (
+            <option key={city.id} value={city.name}>
+              {city.name}
+            </option>
+          );
+        })}
       </select>
+      <button onClick={() => confirmSelection()}>Confirm</button>
       <h1>{city.name}</h1>
       <SimpleGrid minChildWidth="400px" spacing={10}>
         {list.map((day) => {
